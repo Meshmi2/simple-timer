80bb0cb00dc745c1a21a47397704f54f5a730c49
massimo trovati <massimotrovati92@gmail.com>
2019-02-04 13:52:56 +0100

    simple timer console app
    simple timer console application for new developer :)

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+{
+            //Indicates what appears on the screen
+            Console.Write("This window will automatically close between... ");
+            //Indicates a second from 10 to 0
+            for (int a = 10; a >= 0; a--)
+            {
+                //Indicates the position of timer
+                Console.CursorLeft = 50;
+                //Indicates what appears on the screen: a
+                Console.Write("{0} ", a);
+                //Indicates the pause in milliseconds
+                System.Threading.Thread.Sleep(1000);
+   }
+}
+    }
+}
